1371f9dfd0977b83f3011fce6e152315d193d6f1
Ankit Verma <lokdeshtv@gmail.com>
2022-11-02 17:00:40 +0530

    gasv

--- models/adminModels/categoryModel.js
+++ models/adminModels/categoryModel.js
@@ -7,7 +7,6 @@ const categoryModel = mongoose.Schema({
   },
   sortOrder: {
     type: Number,
-    required: [true, "Sort order is required"],
   },
   showInMenu: {
     type: String,
@@ -19,27 +18,21 @@ const categoryModel = mongoose.Schema({
   },
   englishName: {
     type: String,
-    required: [true, "English name is required"], 
   },
   hindiName: {
     type: String,
-    required: [true, "Hindi name is required"],
   },
   startingAlphabet: {
     type: String,
-    required: [true, "Starting alphabet is required"],
   },
   categoryUrl: {
     type: String,
-    required: [true, "Category url is required"],
   },
   metaTitle: {
     type: String,
-    required: [true, "Meta title is required"],
   },
   metaDescription: {
     type: String,
-    required: [true, "Meta description is required"],
   },
 
   news: [
